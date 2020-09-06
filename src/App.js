12b449e8fa55ac6e4137183ff374b2805c236910
mankim-code <mankim.john@gmail.com>
2020-09-06 10:15:35 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+import CheckBox from "./components/CheckBox.js";
+
+function App() {
+  return (
+    <div className="todo-list">
+      <CheckBox />
+      <CheckBox />
+      <CheckBox />
+      <CheckBox />
+    </div>
+    
+  )
+}
+
+
+export default App;
---
+++ src/components/CheckBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function CheckBox(){
+    return(
+        <div className="todo-item">
+        <input type="checkbox" />
+        <p className="input-text">Going to the market</p>
+
+        </div>
+    )
+}   
+export default CheckBox
